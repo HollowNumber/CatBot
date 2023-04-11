715247d2595dc689b6eda784817013836acc4f78
HollowNumber <mikkel.helding@gmail.com>
2023-04-11 15:08:30 +0200

    Initial commit

---
+++ deps.ts
@@ -0,0 +1,3 @@
+export * from "https://deno.land/std@0.181.0/encoding/toml.ts";
+export * from "https://deno.land/x/discordeno@latest/mod.ts";
+
---
+++ main_test.ts
@@ -0,0 +1,3 @@
+import { assertEquals } from "https://deno.land/std@0.182.0/testing/asserts.ts";
+import { add } from "./mod.ts";
+
